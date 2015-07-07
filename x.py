b7748643dc11bd352966a40b39ba501f7ad5aa42
Jared Evans <jnevans@gmail.com>
2015-07-07 12:20:01 -0400

    added sudo code

--- x.py
+++ x.py
@@ -3,20 +3,31 @@
 import stat, sys, os, string, commands
 
 try:
+
+#    user = os.getenv("SUDO_USER")
+#    if user is None:
+#      print "This program need 'sudo'"
+#      sys.exit(1)
+
     html_dir = raw_input("Enter the top-level directory for installation of multiple sites (defaults to /var/www):\n")
 
     if not html_dir:
  	html_dir = '/var/www'
 
-    commandString = "ls " + html_dir 
-    commandOutput = commands.getoutput(commandString)
-    findResults = string.split(commandOutput, "\n")
+    if os.path.isdir(html_dir):
+      commandString = "ls " + html_dir 
+      commandOutput = commands.getoutput(commandString)
+      findResults = string.split(commandOutput, "\n")
 
-    print "Lines:"
-    print commandOutput
-    print "================================"
-    for line in findResults:
-	print line
+      print "Lines:"
+      print commandOutput
+      print "================================"
+      for line in findResults:
+        print line
+    else:
+      print "This home directory doesn't exist on the system...Exiting now."
+      sys.exit(1) 
+
 except:
     print "There was a problem - check the message above"
 
