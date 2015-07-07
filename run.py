9622fc718ab8ddd1cf472dc1cfac4ac7acafc7f8
Jared Evans <jnevans@gmail.com>
2015-07-07 14:04:35 -0400

    Added domain input.

--- run.py
+++ run.py
@@ -29,6 +29,17 @@ try:
       print "This home directory doesn't exist on the system...Exiting now."
       sys.exit(1) 
 
+    domains_input = []
+    entry = raw_input("Enter the domains, one per line. Enter a blank line to quit: \n")
+
+    while entry:
+      domains_input.append(entry)
+      entry = raw_input("")
+
+    print "Setting up the domains: "  
+    for domain in domains_input:
+      print domain
+
 except:
     print "There was a problem - check the message above"
 
