8f65f2781d45e9cad0779751d0f4718db25e56e8
Jared Evans <jnevans@gmail.com>
2015-07-07 12:05:10 -0400

    check html_dir is empty or not

--- x.py
+++ x.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python
+
 import stat, sys, os, string, commands
 
 try:
-    html_dir = raw_input("Enter the top-level directory for installation of multiple sites:\n")
+    html_dir = raw_input("Enter the top-level directory for installation of multiple sites (defaults to /var/www):\n")
+
+    if not html_dir:
+ 	html_dir = '/var/www'
+
     commandString = "ls " + html_dir 
     commandOutput = commands.getoutput(commandString)
     findResults = string.split(commandOutput, "\n")
